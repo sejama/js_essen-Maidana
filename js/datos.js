fbfa3c64172e7d5ca57d616082a6de88c47e1db3
sejama <seba_maidana_3@hotmail.com>
2022-07-26 11:07:41 -0300

    proyecto final

--- js/datos.js
+++ js/datos.js
@@ -1,12 +1,26 @@
+if (window.location.href.includes("ingresar.html")) {
+    fetch('../json/usuarios.json') // Exito
+        .then(response => response.json())  // convertir a json
+        .then(json => //Cargamos los usuarios para poder ingresar o conprobar que si no existe para crear
+            !localStorage.usuarios ? localStorage.setItem("usuarios", JSON.stringify(json)) : null)
+        .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
 
-fetch('../json/usuarios.json') // Exito
-    .then(response => response.json())  // convertir a json
-    .then(json => //Cargamos los usuarios para poder ingresar o conprobar que si no existe para crear
-        !localStorage.usuarios ? localStorage.setItem("usuarios", JSON.stringify(json)) : null)
-    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+    fetch('../json/comentarios.json')
+        .then(response => response.json())  // convertir a json
+        .then(json => //cargamos los datos al localstorage
+            !localStorage.comentarios ? localStorage.setItem("comentarios", JSON.stringify(json)) : null)
+        .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+}
+if (window.location.href.includes("index.html")) {
+    fetch('./json/usuarios.json') // Exito
+        .then(response => response.json())  // convertir a json
+        .then(json => //Cargamos los usuarios para poder ingresar o conprobar que si no existe para crear
+            !localStorage.usuarios ? localStorage.setItem("usuarios", JSON.stringify(json)) : null)
+        .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
 
-fetch('../json/comentarios.json')
-    .then(response => response.json())  // convertir a json
-    .then(json => //cargamos los datos al localstorage
-        !localStorage.comentarios ? localStorage.setItem("comentarios", JSON.stringify(json)) : null)
-    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+    fetch('./json/comentarios.json')
+        .then(response => response.json())  // convertir a json
+        .then(json => //cargamos los datos al localstorage
+            !localStorage.comentarios ? localStorage.setItem("comentarios", JSON.stringify(json)) : null)
+        .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+}
